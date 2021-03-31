6ab702f1815e31de286b40f5ebd4a2d1196a32b5
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-03-31 14:42:18 -0300

    counter update

--- app/models/answear.rb
+++ app/models/answear.rb
@@ -7,7 +7,7 @@ class Answear < ApplicationRecord
   validates_length_of :description, minimum: 5, allow_blank: false
   # after_validation :geocode, if: :coordinates_changed?
   scope :by_id,          -> (id) { find_by("id = ?", id) }
-  scope :by_user,        -> (id) { find_by("user_id = ?", id) }
+  scope :by_user,        -> (id) { where("user_id = ?", id) }
   scope :by_quiz_id,     -> (id) { where("quiz_id = ?", id) }
   scope :by_question_id, -> (id) { where("question_id = ?", id) }
 
